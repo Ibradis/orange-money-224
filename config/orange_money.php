62fd88641b165c901910fa7fafb227afeca7e3b0
ibradis <soryd70@gmail.com>
2025-04-02 16:27:38 +0000

    first commit

---
+++ config/orange_money.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'client_id' => env('ORANGE_CLIENT_ID'),
+    'client_secret' => env('ORANGE_CLIENT_SECRET'),
+    'merchant_key' => env('ORANGE_MERCHANT_KEY'),
+    'return_url' => env('ORANGE_RETURN_URL'),
+    'cancel_url' => env('ORANGE_CANCEL_URL'),
+    'notif_url' => env('ORANGE_NOTIF_URL'),
+];
---
+++ src/OrangeMoney.php
@@ -0,0 +1,58 @@
+<?php
+namespace Ibrahima\OrangeMoney;
+
+use GuzzleHttp\Client;
+
+class OrangeMoney
+{
+    protected $client;
+    protected $config;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->config = config('orange_money');
+    }
+
+    public function getAccessToken()
+    {
+        $response = $this->client->post('https://api.orange.com/oauth/v3/token', [
+            'headers' => [
+                'Authorization' => 'Basic '.base64_encode($this->config['client_id'].':'.$this->config['client_secret']),
+                'Content-Type' => 'application/x-www-form-urlencoded',
+                'Accept' => 'application/json',
+            ],
+            'form_params' => [
+                'grant_type' => 'client_credentials'
+            ]
+        ]);
+
+        return json_decode($response->getBody(), true)['access_token'];
+    }
+
+    public function createPayment($orderId, $amount, $reference)
+    {
+        $token = $this->getAccessToken();
+
+        $response = $this->client->post('https://api.orange.com/orange-money-webpay/dev/v1/webpayment', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+            'json' => [
+                'merchant_key' => $this->config['merchant_key'],
+                'currency' => 'OUV',
+                'order_id' => $orderId,
+                'amount' => $amount,
+                'return_url' => $this->config['return_url'],
+                'cancel_url' => $this->config['cancel_url'],
+                'notif_url' => $this->config['notif_url'],
+                'lang' => 'fr',
+                'reference' => $reference,
+            ]
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+}
